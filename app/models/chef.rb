1070e852963c5566fb8c85ea09749ff305f690d7
Kien Truong <truon01@gmail.com>
2015-11-21 20:05:21 +0000

    create chefs resources and add authentication

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -6,5 +6,7 @@ class Chef < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false },
   format: { with: VALID_EMAIL_REGEX }
+  
+  has_secure_password
 end
 
